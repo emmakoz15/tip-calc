c2419d5733b54b2bb08ef174021317d4a8bcc31c
emmakoz15 <emmakozlovsky@gmail.com>
2024-01-23 10:30:07 -0500

    finished js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// main function
 function calculateAndDisplay(){
     //get input values from html
     let bill = parseFloat(document.getElementById('totalBill').value)
@@ -12,12 +13,36 @@ function calculateAndDisplay(){
   // Display the results in the HTML document
   document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
   document.getElementById('totalBillResult').innerText =
-    'Total Bill: $' + totalBill.toFixed(2)
+    'Total Bill: $' + totalBill.toFixed(2) //.toFixed(2) is fixing the digit yto only 2 decimal places
   document.getElementById('amtPerPersonResult').innerText =
     'Amount per person: $' + amtPerPers.toFixed(2)
 }
 
+// helper function to calc tip based on bill and service
+function calculateTip(bill, quality){ // quality does not need to be 'serviceQuality it is an placeholder does not actually assign values pass through value
+    if(quality === 'Great'){
+        return bill * .2
+    } else if (quality === 'Good'){
+        return bill * 0.15
+    } else if (quality === 'poor'){
+        return bill * 0.10
+    } else {
+        return 0
+    }
+}
 
+
+// helper function to calc total bill with tip and bill
+function calculateTotalBill(bill, tip){
+    return bill + tip
+
+}
+
+// helper function to calc total per person
+
+function calculateAmtPerPers(totalBill, numOfPpl){
+    return totalBill/ numOfPpl
+}
 
 
 
